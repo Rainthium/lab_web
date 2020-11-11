e8e83be7dc08d5c4a7c4dc7ce6d2eddcb52c4a7b
Kirill <kilakill24@gmail.com>
2020-11-11 20:30:40 +0400

    Сделал удаление файлов после удаления статьи

--- vendor/deleteArticle.php
+++ vendor/deleteArticle.php
@@ -4,7 +4,12 @@ require_once 'connect.php';
 
 $article_id = mysqli_real_escape_string($connect, $_POST['article_id']);
 
+$check_article = mysqli_query($connect, "SELECT * FROM `articles` WHERE `id` = '$article_id'");
+$article = mysqli_fetch_assoc($check_article);
+
 mysqli_query($connect, "DELETE FROM `articles` WHERE `articles`.`id` = '$article_id'");
+
+unlink('../'.$article['image']);
 
 header('Location: ../authorArticles.php?author_id=' . $_SESSION['user']['id']);
 ?>
